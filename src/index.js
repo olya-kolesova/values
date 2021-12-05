cc24732f3952a651322ae6c327084d1966cff08a
Olga Kolesova <olka.kolesova@gmail.com>
2021-12-05 13:03:08 +0000

    copied task

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from 'rect';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Validator />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/"
+        />
+      </div>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.querySelector('#root'));
