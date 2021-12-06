8a1079171a43334bd83774cbd9d1905623df56d0
Olga Kolesova <olka.kolesova@gmail.com>
2021-12-06 22:02:08 +0000

    added validator

--- src/Validator.js
+++ src/Validator.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 class Validator extends React.Component {
-  <>
+  constructor(props) {
+    super(props);
+    this.state = { password: ''};
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label htmlFor=""></label>
+            <input
+              type="password"
+            />
+          </div>
+          {this.state.password.length < 4 ? 'Password must be at least 4 characters' : ''}
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Validator;
